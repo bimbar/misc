b898504876c967d8ed2be63f335bc6930fdcff35
Markus Peter <bimbar@users.noreply.github.com>
2024-12-22 19:38:10 +0100

    Update hdr.sh

--- hdr.sh
+++ hdr.sh
@@ -2,13 +2,28 @@
 # Credits to https://www.reddit.com/r/kde/comments/1bcf0ak/toggling_hdr_via_shortcut_or_command/
 # I added the brightness, which is fairly necessary for this kind of switch
 
-if [ "$(kscreen-doctor -o | ansi2txt | grep -cm1 "HDR: enabled"| cut -d= -f2)" -ge 1 ];
+usage() { echo "Usage: $0 <on|off>" 1>&2; exit 1; }
+
+action=${1};
+
+if [ -z $action ];
 then
-  kscreen-doctor output.DP-2.hdr.disable;
-  kscreen-doctor output.DP-2.wcg.disable;
-  kscreen-doctor output.DP-2.brightness.30;
+  if [ "$(kscreen-doctor -o | ansi2txt | grep -cm1 "HDR: enabled")" -ge 1 ];
+  then
+    action="off";
+  else
+    action="on";
+  fi;
+fi;
+
+if [ ${action} == "on" ];
+then
+  echo "Enabling HDR"
+  kscreen-doctor output.DP-1.wcg.enable output.DP-1.hdr.enable output.DP-1.brightness.100;
+elif [ ${action} == "off" ];
+then
+  echo "Disabling HDR"
+  kscreen-doctor output.DP-1.wcg.disable output.DP-1.hdr.disable output.DP-1.brightness.25;
 else
-  kscreen-doctor output.DP-2.wcg.enable;
-  kscreen-doctor output.DP-2.hdr.enable;
-  kscreen-doctor output.DP-2.brightness.100;
+  usage;
 fi;
